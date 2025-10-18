4fc9a4c3d682e3cc3e2c456a83f98107cee945d0
anthow <anthonyenglebert@gmail.com>
2025-10-19 00:43:33 +0200

    /

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,13 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
-import adapter from '@sveltejs/adapter-netlify'
+import adapter from '@sveltejs/adapter-static'
 
 export default {
   preprocess: vitePreprocess(),
   kit: {
     adapter: adapter({
-      edge: false,
-      split: false
+      pages: 'build',
+      assets: 'build',
+      fallback: 'index.html'
     })
   }
 }
